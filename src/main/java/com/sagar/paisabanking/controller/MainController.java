e04da771120013f2393b89299e86116b04608db6
Sagar Niroula <sagarniroula112@gmail.com>
2025-01-15 18:37:11 +0545

    Fund receive not showing solved.

--- src/main/java/com/sagar/paisabanking/controller/MainController.java
+++ src/main/java/com/sagar/paisabanking/controller/MainController.java
@@ -1,14 +1,21 @@
 package com.sagar.paisabanking.controller;
 
 import com.sagar.paisabanking.model.User;
+import com.sagar.paisabanking.service.UserService;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/")
     private String index() {
@@ -18,9 +25,14 @@ public class MainController {
     @GetMapping("/dashboard")
     private String dashboard(HttpSession session, Model model, HttpServletRequest request) {
 
-        User activeUser = (User) session.getAttribute("activeUser");
+        User sessionUser = (User) session.getAttribute("activeUser");
 
-        if (activeUser != null) {
+        if (sessionUser != null) {
+            User activeUser = userService.getUserById(sessionUser.getId());
+
+            // Update the session with the latest user object
+            session.setAttribute("activeUser", activeUser);
+
             model.addAttribute("request", request);
             model.addAttribute("username", activeUser.getUsername());
             model.addAttribute("balance", activeUser.getAccount().getBalance());
